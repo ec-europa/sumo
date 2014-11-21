7426b30bc3c56c10be28dc21a94a79c04dbc825d
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-11-21 17:12:33 +0100

    refactoring
    Former-commit-id: a3c53bb251cf09afdf6f25ebddbacb98becab56e

--- tags/SUMO_WIN64_1.0.0b5/GeoImage/src/main/java/org/geoimage/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package org.geoimage.utils;
-
-public class Constant {
-  public static int OVERVIEW_SIZE=256;
-  public static double OVERVIEW_SIZE_DOUBLE=256.d;
-  
-  public static int TILE_SIZE = 256;
-  
-}
