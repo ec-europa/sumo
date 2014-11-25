b7ad68e1bc667e4ca739fe3e7910dbf3d8725c83
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-11-25 16:43:12 +0100

    String constant for define the preferences

--- trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/util/Constant.java
+++ trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/util/Constant.java
@@ -8,6 +8,54 @@ public class Constant {
   public static int TILE_SIZE = 256;
  
   
+//vds analysis preferences
+  public static final String PREF_DISPLAY_PIXELS = "VDS Analaysis - Display detected pixels";
+  public static final String PREF_DISPLAY_BANDS = "VDS Analaysis - Display all bands detection results";
+  public static final String PREF_TARGETS_COLOR_BAND_0 = "VDS Analaysis - Target Color - Band 0";
+  public static final String PREF_TARGETS_SIZE_BAND_0 = "VDS Analaysis - Target Size - Band 0";
+  public static final String PREF_TARGETS_SYMBOL_BAND_0 = "VDS Analaysis - Target Symbol - Band 0";
+  public static final String PREF_TARGETS_COLOR_BAND_1 = "VDS Analaysis - Target Color - Band 1";
+  public static final String PREF_TARGETS_COLOR_BAND_2 = "VDS Analaysis - Target Color - Band 2";
+  public static final String PREF_TARGETS_COLOR_BAND_3 = "VDS Analaysis - Target Color - Band 3";
+  public static final String PREF_TARGETS_SIZE_BAND_1 = "VDS Analaysis - Target Size - Band 1";
+  public static final String PREF_TARGETS_SIZE_BAND_2 = "VDS Analaysis - Target Size - Band 2";
+  public static final String PREF_TARGETS_SIZE_BAND_3 = "VDS Analaysis - Target Size - Band 3";
+  public static final String PREF_TARGETS_SYMBOL_BAND_1 = "VDS Analaysis - Target Symbol - Band 1";
+  public static final String PREF_TARGETS_SYMBOL_BAND_2 = "VDS Analaysis - Target Symbol - Band 2";
+  public static final String PREF_TARGETS_SYMBOL_BAND_3 = "VDS Analaysis - Target Symbol - Band 3";
+  public static final String PREF_TARGETS_COLOR_BAND_MERGED = "VDS Analaysis - Target Color - Band Merged";
+  public static final String PREF_TARGETS_SIZE_BAND_MERGED = "VDS Analaysis - Target Size - Band Merged";
+  public static final String PREF_TARGETS_SYMBOL_BAND_MERGED = "VDS Analaysis - Target Symbol - Band Merged";
+  public static final String PREF_BUFFERING_DISTANCE = "VDS Analaysis - Buffering Distance in pixels";
+  public static final String PREF_AGGLOMERATION_METHODOLOGY = "VDS Analaysis - Agglomeration Method";
+  public static final String PREF_NEIGHBOUR_DISTANCE = "VDS Analaysis - Neighbours distance";
+  public static final String PREF_NEIGHBOUR_TILESIZE = "VDS Analaysis - Neighbours tile size";
+  public static final String PREF_REMOVE_LANDCONNECTEDPIXELS = "VDS Analaysis - Remove pixels connected to land in neighbour mode";
 
+  //preferences for import image and vector actions
+  public static final String PREF_LASTIMAGE = "Last Image";
+  public static final String PREF_LASTVECTOR = "Last Vector";
+  public static final String PREF_IMAGE_FOLDER = "Image Default Folder";
+  public static final String PREF_USE_GEOLOCATIONCORRECTION = "Image - use geolocation correction file";
+  
+  
+  //cache folder preference 
+  public final static String PREF_CACHE = "Cache Folder";
+  
+  public static final String GOOGLE_CHART_API="Google Chart format";
+  public static final String NUM_HISTOGRAM_CLASSES="Number of Histogram Classes";
+  
+  //VDSAnalysisVersionDialog
+  public static final String PREF_VERSION = "PostGIS - VDS Analysis Version";
+  
+  //Geometric interactivelayer
+  public static final String PREF_AZIMUTH_GEOMETRYTAG = "Azimuth Geometry";
+  public static final String PREF_AZIMUTH_GEOMETRYCOLOR = "VDS Analysis - Azimuth Geometry Color";
+  public static final String PREF_AZIMUTH_GEOMETRYLINEWIDTH = "VDS Analysis - Azimuth Geometry Line Width";
+
+  
+  public static final String PREF_COASTLINE_DEFAULT_LAND_MASK = "Default land mask";
+  public static final String PREF_COASTLINES_FOLDER = "Coastline Shape folders";
+  
   
 }
