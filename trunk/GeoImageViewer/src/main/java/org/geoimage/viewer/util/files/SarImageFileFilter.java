fccb35719106bde9b67c0c757578f74d3e72692b
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-12-02 16:10:30 +0100

    File util to manage images
    Former-commit-id: cc4359702f4233188bdb7a90911342583b5f0119

---
+++ trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/util/files/SarImageFileFilter.java
@@ -0,0 +1,25 @@
+package org.geoimage.viewer.util.files;
+
+import java.io.File;
+
+public class SarImageFileFilter  implements java.io.FileFilter{
+		private final String[] patternRec={".*\\.h5",".*manifest\\.safe.*",".*product\\.xml.*",".*\\.N1",".*TS.*xml.*",".*TDX.*xml.*"};
+		
+		@Override
+		public boolean accept(File pathname) {
+			boolean ok=false;
+			for(int i=0;i<patternRec.length && ok==false;i++){
+				String fileName=pathname.getName();
+				if(fileName.matches(patternRec[i])){
+					ok= true;
+				}	
+			}
+			return ok;
+		}
+		
+		
+
+		
+		
+		
+}
