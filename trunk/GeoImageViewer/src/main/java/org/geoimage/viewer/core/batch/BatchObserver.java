26f5ffff4d0c491d8ee75f1ac620e914ce7a1f87
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-12-02 16:17:28 +0100

    listener/observer batch events
    Former-commit-id: a7462417f5265f47ba39d2d97009966ac246deef

---
+++ trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/core/batch/BatchObserver.java
@@ -0,0 +1,52 @@
+package org.geoimage.viewer.core.batch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BatchObserver {
+
+	private static BatchObserver instance;
+    private List<BachListener> listeners;
+
+    private BatchObserver() {
+        listeners = new  ArrayList<BachListener>();
+    }
+
+    public static synchronized BatchObserver getInstance() {
+        if(instance == null) {
+            instance = new BatchObserver();
+        }
+
+        return instance;
+    }
+
+    public synchronized void addBachListener(BachListener listener) {
+        // Check if listener already exists in list, skip if it does
+        for (BachListener listListener : listeners) {
+            if(listListener.equals(listener)) {
+                return;
+            }
+        }
+
+        listeners.add(listener);
+    }
+
+    public synchronized void removeBachListener(BachListener listener) {
+    	if(listeners.size()>0)
+    		listeners.remove(listener);
+    }
+
+    /**
+     * 
+     * @param image
+     * @param msg
+     */
+    public void notifyError(String image,String msg) {
+        for (BachListener listener : listeners) {
+            listener.notifyError(image, msg);
+        }
+    }
+
+	
+	
+}
