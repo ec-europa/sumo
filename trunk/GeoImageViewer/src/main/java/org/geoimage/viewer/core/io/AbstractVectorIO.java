583d5660ce8f670ac7425a4ec840c247a6dfc5e1
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-12-02 16:45:35 +0100

    refactoring
    Former-commit-id: fb56ee7acd4c537d2f16368d2b6e5cb143c89eae

--- trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/core/io/AbstractVectorIO.java
+++ trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/core/io/AbstractVectorIO.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.geoimage.def.GeoImageReader;
-import org.geoimage.def.GeoTransform;
 import org.geoimage.viewer.core.api.GeometricLayer;
 
 /**
