6c84b53bfee680f6852c3ec8ae0406254f7b9198
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2014-12-16 16:12:27 +0100

    Correction to analyze Cosmoskymed Images in batch mode
    Former-commit-id: 6aff2bb644739a6c56a13a4052f6cf2df0052851

--- trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/core/batch/SingleBatchAnalysis.java
+++ trunk/GeoImageViewer/src/main/java/org/geoimage/viewer/core/batch/SingleBatchAnalysis.java
@@ -23,19 +23,22 @@ public class SingleBatchAnalysis extends AbstractBatchAnalysis {
 	protected void startAnalysis(){
 		//crate the reader
 		List<GeoImageReader> readers =  GeoImageReaderFactory.createReaderForName(params.pathImg);
-		SarImageReader reader=(SarImageReader) readers.get(0);
 		
-		GeometricLayer gl=null;
-		if(params.shapeFile!=null)
-			gl=readShapeFile(reader);
-		
-		IMask[] masks = null;
-		if(params.buffer!=0&&gl!=null){
-			masks=new IMask[1];
-			masks[0]=FactoryLayer.createMaskLayer("buffered", FactoryLayer.TYPE_COMPLEX, params.buffer, reader, gl);
+		for(GeoImageReader r:readers){
+			SarImageReader reader=(SarImageReader) r;
+			
+			GeometricLayer gl=null;
+			if(params.shapeFile!=null)
+				gl=readShapeFile(reader);
+			
+			IMask[] masks = null;
+			if(params.buffer!=0&&gl!=null){
+				masks=new IMask[1];
+				masks[0]=FactoryLayer.createMaskLayer("buffered", FactoryLayer.TYPE_COMPLEX, params.buffer, reader, gl);
+			}	
+			
+			analizeImage(reader,masks,params);
+			saveResults(reader.getName(),masks,reader);
 		}	
-		
-		analizeImage(reader,masks,params);
-		saveResults(reader.getName(),masks,reader);
 	}	
 }
