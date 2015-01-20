d7805f6a473514da4965bbc9190b95946e11f018
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-01-20 17:21:35 +0100

    Parameter for analysis exclusion pixel

--- trunk/GeoImageAnalysis/src/main/java/org/geoimage/analysis/VDSAnalysis.java
+++ trunk/GeoImageAnalysis/src/main/java/org/geoimage/analysis/VDSAnalysis.java
@@ -129,6 +129,8 @@ public class VDSAnalysis {
             this.progressBar.setMaximum(verTiles);
         }
             
+        int dy=sizeTile[1];
+        
         for (int j = 0; j < verTiles; j++) {
             
             // update the progress bar value
@@ -136,43 +138,60 @@ public class VDSAnalysis {
             {
                 this.progressBar.setCurrent(j);
             }
+            if(j==verTiles-1){
+            	//the last tiles have more pixels so we need to calculate the real size
+            	dy=gir.getHeight()-((verTiles-1)*sizeTile[1]);
+            }
+            
             
             tile[0][0] = 0;
             tile[0][1] = gir.getWidth();
-            tile[1][0] = j * sizeTile[1];
-            tile[1][1] = tile[1][0] + sizeTile[1];
+            tile[1][0] = j * dy;
+            tile[1][1] = tile[1][0] + dy;
 
+            int dx=sizeTile[0];
+            
+            
             for (int i = 0; i < horTiles; i++) {
-
-                tile[0][0] = i * sizeTile[0];
-                tile[0][1] = tile[0][0] + sizeTile[0];
+            	if(i==horTiles-1){
+            		//the last tiles have more pixels so we need to calculate the real size
+                	dx=gir.getWidth()-((horTiles-1)*dx);
+                }
+            	
+                tile[0][0] = i * dx;
+                tile[0][1] = tile[0][0] + dx;
                 if (mask == null || mask.length == 0 || mask[0] == null || !intersects(tile)) {
-                    kdist.setImageData(gir, tile[0][0], tile[1][0], 1, 1, sizeTile[0], sizeTile[1]);
-                    kdist.estimate(null);
-                    double[][][] thresh = kdist.getDetectThresh();
-                    tileStat[j] = kdist.getTileStat()[0];
-                    int[] data = gir.readTile(tile[0][0], tile[1][0], sizeTile[0], sizeTile[1]);
+                	kdist.setImageData(gir, tile[0][0], tile[1][0], 1, 1, dx, dy);
+                	kdist.estimate(null);
 
-                    for (int k = 0; k < sizeTile[1]; k++) {
-                        for (int h = 0; h < sizeTile[0]; h++) {
+                	double[][][] thresh = kdist.getDetectThresh();
+                    tileStat[j] = kdist.getTileStat()[0];
+                    
+                    
+                    int[] data = gir.readTile(tile[0][0], tile[1][0], dx, dy);
+                    
+                    double threshWindowsVals[]=calcThreshWindowVals(significance, thresh[0][0]);
+
+                    for (int k = 0; k < dy; k++) {
+                        for (int h = 0; h < dx; h++) {
                             int subwindow = 1;
-                            if (h < sizeTile[0] / 2) {
-                                if (k < sizeTile[1] / 2) {
+                            if (h < dx / 2) {
+                                if (k < dy / 2) {
                                     subwindow = 1;
                                 } else {
                                     subwindow = 3;
                                 }
                             } else {
-                                if (k < sizeTile[1] / 2) {
+                                if (k < dy / 2) {
                                     subwindow = 2;
                                 } else {
                                     subwindow = 4;
                                 }
                             }
-                            int pix = data[k * sizeTile[0] + h];
+                            int pix = data[k * dx + h];
                             // if (pix > thresh[i][0][subwindow] * (significance - (significance - 1.)	/ thresh[i][0][5])) {
                             // Modified condition from S = ((pix/mean) - 1)/(t_p - 1) where T_window = t_p * mean
-                            if (pix > (significance * (thresh[0][0][5] - 1.0) + 1.0) * thresh[0][0][subwindow] / thresh[0][0][5]) {
+                            if (pix >threshWindowsVals[subwindow-1] ) {
                                 dpixels.add(h + tile[0][0], k + tile[1][0], pix, thresh[0][0][subwindow] / thresh[0][0][5], thresh[0][0][0] * thresh[0][0][subwindow] / thresh[0][0][5], thresh[0][0][5], bandname);
                             }
 
@@ -185,45 +204,48 @@ public class VDSAnalysis {
                         continue;
                     // create raster mask
                     Raster rastermask = null;
-                    rastermask = (mask[0].rasterize(new Rectangle(tile[0][0], tile[1][0], sizeTile[0], sizeTile[1]), -tile[0][0], -tile[1][0], 1.0)).getData();
+                    rastermask = (mask[0].rasterize(new Rectangle(tile[0][0], tile[1][0], dx, dy), -tile[0][0], -tile[1][0], 1.0)).getData();
                     //Read pixels for the area and check there are enough sea pixels
                     int[] maskdata = rastermask.getPixels(0, 0, rastermask.getWidth(), rastermask.getHeight(), (int[])null);
                     int pixelcount = 0;
                     for(int count = 0; count < maskdata.length; count++)
                         pixelcount += maskdata[count];
-                    //System.out.println("Mask pixels at " + (new Rectangle(tile[0][0], tile[1][0], sizeTile[0], sizeTile[1])).toString() + ":" + pixelcount);
+                    //System.out.println("Mask pixels at " + (new Rectangle(tile[0][0], tile[1][0], dx, dy)).toString() + ":" + pixelcount);
                     if(((double)pixelcount / maskdata.length) < 0.7)
                     {
                         // if there are pixels to estimate, calculate statistics using the mask
-                        kdist.setImageData(gir, tile[0][0], tile[1][0], 1, 1, sizeTile[0], sizeTile[1]);
+                        kdist.setImageData(gir, tile[0][0], tile[1][0], 1, 1, dx, dy);
                         kdist.estimate(rastermask);
                         double[][][] thresh = kdist.getDetectThresh();
                         tileStat[j] = kdist.getTileStat()[0];
-                        int[] data = gir.readTile(tile[0][0], tile[1][0], sizeTile[0], sizeTile[1]);
+                        
+                        double threshWindowsVals[]=calcThreshWindowVals(significance, thresh[0][0]);
+                        
+                        int[] data = gir.readTile(tile[0][0], tile[1][0], dx, dy);
 
-                        for (int k = 0; k < sizeTile[1]; k++) {
-                            for (int h = 0; h < sizeTile[0]; h++) {
+                        for (int k = 0; k < dy; k++) {
+                            for (int h = 0; h < dx; h++) {
                                 // check pixel is in the sea
                                 if(rastermask.getSample(h, k, 0) == 0)
                                 {
                                     int subwindow = 1;
-                                    if (h < sizeTile[0] / 2) {
-                                        if (k < sizeTile[1] / 2) {
+                                    if (h < dx / 2) {
+                                        if (k < dy / 2) {
                                             subwindow = 1;
                                         } else {
                                             subwindow = 3;
                                         }
                                     } else {
-                                        if (k < sizeTile[1] / 2) {
+                                        if (k < dy / 2) {
                                             subwindow = 2;
                                         } else {
                                             subwindow = 4;
                                         }
                                     }
-                                    int pix = data[k * sizeTile[0] + h];
+                                    int pix = data[k * dx + h];
                                     // if (pix > thresh[i][0][subwindow] * (significance - (significance - 1.)	/ thresh[i][0][5])) {
                                     // Modified condition from S = ((pix/mean) - 1)/(t_p - 1) where T_window = t_p * mean
-                                    if (pix > (significance * (thresh[0][0][5] - 1.0) + 1.0) * thresh[0][0][subwindow] / thresh[0][0][5]) {
+                                    if (pix > threshWindowsVals[subwindow-1]) {
                                         dpixels.add(h + tile[0][0], k + tile[1][0], pix, thresh[0][0][subwindow] / thresh[0][0][5], thresh[0][0][0] * thresh[0][0][subwindow] / thresh[0][0][5], thresh[0][0][5], bandname);
                                     }
                                 }
@@ -234,11 +256,29 @@ public class VDSAnalysis {
                 }
             }
             System.out.println(j + "/" + verTiles);
+           // System.out.println("Detected Pixels for Tile "+j+ ":"+dpixels.getAllDetectedPixels().size());
         }
-        System.out.println();
+        System.out.println("Detected Pixels Total :"+dpixels.getAllDetectedPixels().size());
         return dpixels;
     }
 
+    /**
+     * 
+     * @param significance
+     * @param thresh
+     * @return
+     */
+    private double[] calcThreshWindowVals(double significance,double[] thresh){
+    	double threshWindowsVals[]=new double[4];
+    	threshWindowsVals[0]=(significance * (thresh[5] - 1.0) + 1.0) * thresh[1] / thresh[5];
+    	threshWindowsVals[1]=(significance * (thresh[5] - 1.0) + 1.0) * thresh[2] / thresh[5];
+    	threshWindowsVals[2]=(significance * (thresh[5] - 1.0) + 1.0) * thresh[3] / thresh[5];
+    	threshWindowsVals[3]=(significance * (thresh[5] - 1.0) + 1.0) * thresh[4] / thresh[5];
+    	
+    	return threshWindowsVals;
+    }
+    
+    
     public boolean intersects(int[][] tile) {
         if (mask == null) {
             return false;
