05690ea98a6119f851b7d4982d792d4971c38d44
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-02-02 18:11:26 +0100

    Black border analysis correction
    Former-commit-id: a86a34e5013715b8cd641708dc20ab16d00e69c5

---
+++ trunk/GeoImageAnalysis/src/main/java/org/geoimage/analysis/ConstantVDSAnalysis.java
@@ -0,0 +1,14 @@
+package org.geoimage.analysis;
+
+public class ConstantVDSAnalysis {
+	 //thresh for S1 vds analisys
+	  public static int THRESH_D_EXTREMES_RATIO=3; 
+	  public static int THRESH_D_EXTREMES_DIFF=5;
+	  public static int THRESH_IS_BORDER=10;
+	  public static double THRESH_MAX_FACTOR=0.2;
+	  public static int THRESH_VALUE_SAFE=50;
+	  
+	  
+	  public static int ROW_TILE_SAMPLES_ARRAY[]={10,50,90}; //10% 50% 90% indicate how many and what rows we want to use as samples for each tile 
+	  
+}
