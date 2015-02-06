93204961d3c1dc3bf5c7b6562ec2c720cc5a8e9a
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-02-06 16:57:06 +0100

    black border tile analisys
    Former-commit-id: b7d8d2c1e7edffc5d417675cff9b062932fe95a1

--- trunk/GeoImageAnalysis/src/main/java/org/geoimage/analysis/BlackBorderAnalysis.java
+++ trunk/GeoImageAnalysis/src/main/java/org/geoimage/analysis/BlackBorderAnalysis.java
@@ -10,18 +10,25 @@ public class BlackBorderAnalysis {
 	
 	private double dAvEtreme1[]=new double[nRowSamples];
 	private double dAvEtreme2[]=new double[nRowSamples];
-	private double dAvEtremeRation[]=new double[nRowSamples];
+	
+	private double dAvEtremeRatio[]=new double[nRowSamples];
 	private double dAvEtremeDiff[]=new double[nRowSamples];
 	
 	
+	private GeoImageReader gir;
 	
 	
-	public BlackBorderAnalysis() {
+	
+	public BlackBorderAnalysis(GeoImageReader gir) {
+		this.gir=gir;
 	}
 		
 	
+	public void analyse(int row,int col) {
+		
+	}
 	
-	private void analyse(GeoImageReader gir) {
+	public void analyse() {
 		
 		//define the size of the tiles
 		int tileSize = (int)(ConstantVDSAnalysis.TILESIZE / gir.getGeoTransform().getPixelSize()[0]);
@@ -42,19 +49,8 @@ public class BlackBorderAnalysis {
         	
         	for (int i = 0; i < 5; i++) {
         		int iniX = startTileX+i * dx;
-        		//riga di campionamento 1 al 10%
-        		int row1=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[0]*(iniY+dy))/100;
-        		//leggo i dati di 1 riga
-        		int[] dataRow1=gir.readTile(iniX, row1, dx,1);
         		
-        		//riga di campionamento 2 al 50%
-        		int row2=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[1]*(iniY+dy))/100;
-        		int[] dataRow2=gir.readTile(iniX, row2, dx,1);
-        		
-        		//riga di campionamento 3 al 90%
-        		int row3=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[2]*(iniY+dy))/100;
-        		int[] dataRow3=gir.readTile(iniX, row3, dx,1);
-        		
+        		analyzeTile(iniX,iniY,dx,dy);
         		
         		
         		startTileX=iniX;
@@ -62,5 +58,27 @@ public class BlackBorderAnalysis {
         	startTileY=iniY;
         }
 	}    
+	
+	private void analyzeTile(int iniX,int iniY,int dx,int dy){
+		//riga di campionamento 1 al 10%
+		int row1=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[0]*(iniY+dy))/100;
+		//leggo i dati di 1 riga
+		int[] dataRow1=gir.readTile(iniX, row1, dx,1);
+		
+		//riga di campionamento 2 al 50%
+		int row2=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[1]*(iniY+dy))/100;
+		int[] dataRow2=gir.readTile(iniX, row2, dx,1);
+		
+		//riga di campionamento 3 al 90%
+		int row3=iniY+(ConstantVDSAnalysis.ROW_TILE_SAMPLES_ARRAY[2]*(iniY+dy))/100;
+		int[] dataRow3=gir.readTile(iniX, row3, dx,1);
+		
+		
+		
+		
+		
+		
+		
+	}
 	
 }
