ab7f1b9e3dff9913464a68dd84da1322641a5835
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-06-03 17:11:54 +0200

    fastmath
    Former-commit-id: f5a3f22d81bd91c25fefd0f6fcd318dd92ff8855

--- GeoLocation/src/main/java/jrc/it/geolocation/api/IInterpolation.java
+++ GeoLocation/src/main/java/jrc/it/geolocation/api/IInterpolation.java
@@ -2,13 +2,11 @@ package jrc.it.geolocation.api;
 
 import java.util.List;
 
-import jrc.it.geolocation.interpolation.HermiteInterpolation;
 import jrc.it.geolocation.metadata.S1Metadata;
 
 public interface IInterpolation {
-	public HermiteInterpolation.InterpolationResult interpolation(double[] subTimesDiffRef, 
-						List<S1Metadata.OrbitStatePosVelox> vpList,
-						double[] timeStampInitSecondsRefPointsInterp,
-						int idxInitTime, int idxEndTime,
-						double deltaT);
+	public void interpolation(double[] subTimesDiffRef,List<S1Metadata.OrbitStatePosVelox> vpList,
+			double timeStampInitSecondsRefPointsInterp[],
+			int idxInitTime, int idxEndTime,
+			double deltaT);
 }
