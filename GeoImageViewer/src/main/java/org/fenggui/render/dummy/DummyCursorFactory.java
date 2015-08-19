4722980075bba02576a53e05893dc5090198bc72
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-08-19 15:25:41 +0200

    --
    Former-commit-id: 75ee2e712220f75848eb618cdc0e43d931b12a19

--- GeoImageViewer/src/main/java/org/fenggui/render/dummy/DummyCursorFactory.java
+++ GeoImageViewer/src/main/java/org/fenggui/render/dummy/DummyCursorFactory.java
@@ -6,59 +6,50 @@ import org.fenggui.render.Cursor;
 import org.fenggui.render.CursorFactory;
 
 public class DummyCursorFactory extends CursorFactory
-{
+{// TODO Check this class!!!
 
 	@Override
 	public Cursor createCursor(int xHotspot, int yHotspot, BufferedImage image) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getDefaultCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getHandCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getMoveCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getTextCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getHorizontalResizeCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getVerticalResizeCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getNWResizeCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Cursor getSWResizeCursor() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
