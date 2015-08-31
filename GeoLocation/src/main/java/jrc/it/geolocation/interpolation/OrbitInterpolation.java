3e508e43936b967cb02b7a2b0216bb46c7d6948e
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-08-31 17:25:51 +0200

    Geolocation convertion bug fix for timeStampInitSeconds in the orbit state vector
    Former-commit-id: 7fb3840e59cb49bd4a440eb00fb8ba413c5538f6

--- GeoLocation/src/main/java/jrc/it/geolocation/interpolation/OrbitInterpolation.java
+++ GeoLocation/src/main/java/jrc/it/geolocation/interpolation/OrbitInterpolation.java
@@ -1,8 +1,6 @@
 package jrc.it.geolocation.interpolation;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.math3.util.FastMath;
