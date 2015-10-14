49fd2d159c1de53281db8a54fa0bd071c774d5a6
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-10-14 11:27:30 +0200

    button label

--- GeoImageViewer/src/main/java/org/geoimage/opengl/control/GLButton.java
+++ GeoImageViewer/src/main/java/org/geoimage/opengl/control/GLButton.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.media.opengl.GLDrawable;
 
 import com.jogamp.opengl.util.awt.TextRenderer;
 
@@ -25,18 +26,32 @@ public class GLButton extends GLWidget{
 
 	@Override
 	public void paint(){
+		// Compute maximum width of text we're going to draw
+		TextRenderer textRenderer = new TextRenderer(new Font(Font.SERIF,0 , 18));
+		
+	    int maxTextWidth = (int) textRenderer.getBounds(this.text).getWidth();
+	    maxTextWidth = Math.min(maxTextWidth+10,size.width);
+		
 		GL2 gl=glContext.getGL2();
 		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
 		gl.glPolygonMode (GL2.GL_FRONT, GL2.GL_LINE_STRIP);
 	    gl.glColor3f(0.5f, 0.5f, 0.5f);
-	    gl.glRecti(this.posx,this.posy,80,20);
-		
-		
-		TextRenderer textRenderer = new TextRenderer(new Font(Font.SERIF,0 , 20));
-		textRenderer.beginRendering(700,300);//size
+
+	    gl.glBegin(GL2.GL_LINE_STRIP);
+	    gl.glVertex2i( this.posx,this.posy);							//x1,y1
+	    gl.glVertex2i( this.posx+maxTextWidth,this.posy );				//x2,y1
+	    gl.glVertex2i( this.posx+maxTextWidth,this.posy +size.height); 	//x2,y2 
+	    gl.glVertex2i( this.posx,this.posy+size.height);				//x1,y2
+	    gl.glVertex2i( this.posx,this.posy);	
+	    gl.glEnd( );
+	    
+	    
+	    GLDrawable draw=gl.getGL().getContext().getGLDrawable();
+		textRenderer.beginRendering(draw.getWidth(),draw.getHeight());
 		textRenderer.setColor(Color.WHITE);
-		textRenderer.setSmoothing(false);
-		textRenderer.draw(this.text,this.posx,this.posy); //text and position
+		textRenderer.setSmoothing(true);
+		textRenderer.draw(this.text,(int)posx,(int)posy+5); //text and position
+		textRenderer.flush();
 		textRenderer.endRendering();
 		
 		gl.glFlush();
--- GeoImageViewer/src/main/java/org/geoimage/opengl/control/TestFrameComponent.java
+++ GeoImageViewer/src/main/java/org/geoimage/opengl/control/TestFrameComponent.java
@@ -50,49 +50,14 @@ public class TestFrameComponent extends JFrame implements GLEventListener{
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		GL gl = drawable.getGL();
-		setCamera(gl, glu, 100);
-		new GLButton(gl,"Test", new Dimension(10,10),0,0).paint(); 
+		new GLButton(gl,"Test", new Dimension(100,20),40,40).paint(); 
 		
-		
-	    /* clear all pixels 
-	    gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-	    gl.glColor3f(0.5f, 0.5f, 0.5f);
-	    gl.glRectf(0.25f,0.25f,0.75f,0.75f);
-	    /*
-	     * draw white polygon (rectangle) with corners at (0.25, 0.25, 0.0) and
-	     * (0.75, 0.75, 0.0)
-	     */
-	    /*gl.glColor3f(0.5f, 0.5f, 0.5f);
-	    gl.glBegin(GL2.GL_POLYGON);
-	    gl.glVertex3f(0.25f, 0.25f, 0.0f);
-	    gl.glVertex3f(0.75f, 0.25f, 0.0f);
-	    gl.glVertex3f(0.75f, 0.75f, 0.0f);
-	    gl.glVertex3f(0.25f, 0.75f, 0.0f);
-	    gl.glEnd();
-	 
 	    /*
 	     * don't wait! start processing buffered OpenGL routines
 	     */
-	   // gl.glFlush();
-		
+	    gl.glFlush();
 	}
 
-	private void setCamera(GL gl, GLU glu, float distance) {
-        GL2 gl2=gl.getGL2();
-		// Change to projection matrix.
-        gl2.glMatrixMode(GL2.GL_PROJECTION);
-        gl2.glLoadIdentity();
-        glu.gluOrtho2D(0,getWidth(),0,getHeight());
-        
-        // Perspective.
-       /* float widthHeightRatio = (float) getWidth() / (float) getHeight();
-        glu.gluPerspective(45, widthHeightRatio, 1, 1000);
-        glu.gluLookAt(0, 0, distance, 0, 0, 0, 0, 1, 0);*/
-		
-        // Change back to model view matrix.
-        gl2.glMatrixMode(GL2.GL_MODELVIEW);
-        gl2.glLoadIdentity();
-    }
 	
 	
 	@Override
