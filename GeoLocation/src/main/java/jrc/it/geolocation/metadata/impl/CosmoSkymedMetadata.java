c8058171342919f1558761fd87ae11c91eb8e32e
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2015-12-04 16:33:53 +0100

    fix geocoding (OrbitInterpolation) problem with new S1

---
+++ GeoLocation/src/main/java/jrc/it/geolocation/metadata/impl/CosmoSkymedMetadata.java
@@ -0,0 +1,49 @@
+package jrc.it.geolocation.metadata.impl;
+
+import jrc.it.geolocation.metadata.IMetadata;
+
+public class CosmoSkymedMetadata implements IMetadata {
+
+	@Override
+	public void initMetaData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setType(String type) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isPixelTimeOrderingAscending() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPixelTimeOrderingAscending(boolean pixelTimeOrderingAscending) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double getNumberOfSamplesPerLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getAntennaPointing() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
