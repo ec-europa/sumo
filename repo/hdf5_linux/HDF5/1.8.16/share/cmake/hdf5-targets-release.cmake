341a6e34963d37e5a91496efd99b07671ce000c5
argenpo <argenpo@debian>
2016-01-07 12:30:27 +0100

    hdf5 linux compiled
    Former-commit-id: f29856a9bc65f564be22e2d030c5b5acb129117d

---
+++ repo/hdf5_linux/HDF5/1.8.16/share/cmake/hdf5-targets-release.cmake
@@ -0,0 +1,59 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "hdf5-static" for configuration "Release"
+set_property(TARGET hdf5-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hdf5-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhdf5.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hdf5-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hdf5-static "${_IMPORT_PREFIX}/lib/libhdf5.a" )
+
+# Import target "hdf5_tools-static" for configuration "Release"
+set_property(TARGET hdf5_tools-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hdf5_tools-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhdf5_tools.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hdf5_tools-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hdf5_tools-static "${_IMPORT_PREFIX}/lib/libhdf5_tools.a" )
+
+# Import target "hdf5_hl-static" for configuration "Release"
+set_property(TARGET hdf5_hl-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hdf5_hl-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhdf5_hl.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hdf5_hl-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hdf5_hl-static "${_IMPORT_PREFIX}/lib/libhdf5_hl.a" )
+
+# Import target "hdf5_cpp-static" for configuration "Release"
+set_property(TARGET hdf5_cpp-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hdf5_cpp-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhdf5_cpp.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hdf5_cpp-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hdf5_cpp-static "${_IMPORT_PREFIX}/lib/libhdf5_cpp.a" )
+
+# Import target "hdf5_hl_cpp-static" for configuration "Release"
+set_property(TARGET hdf5_hl_cpp-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(hdf5_hl_cpp-static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhdf5_hl_cpp.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS hdf5_hl_cpp-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_hdf5_hl_cpp-static "${_IMPORT_PREFIX}/lib/libhdf5_hl_cpp.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
