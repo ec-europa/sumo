a16d22e927850a640404aa3ccd6028a3feb16e3b
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2016-01-12 17:07:11 +0100

    noise floor param merge
    Former-commit-id: 089e9852e403cb30e87d17db207c4e02a2499903

--- SumoCore/src/main/java/org/jrc/sumo/configuration/PlatformConfiguration.java
+++ SumoCore/src/main/java/org/jrc/sumo/configuration/PlatformConfiguration.java
@@ -255,5 +255,11 @@ public class PlatformConfiguration {
     	return (String)prefDB.getConfiguration(Constant.PREF_LASTVECTOR);
     }
     
-    
+    public int getNoiseFloor(int defaultValue){
+    	try{
+    		return prefDB.getPrefNoiseFloor();
+    	}catch(Exception e){
+    		return defaultValue; 
+    	}
+    }
 }
