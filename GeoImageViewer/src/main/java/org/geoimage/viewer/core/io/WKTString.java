433ad831aa7f3c4cdb5212e6a79093148965e7a5
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2016-02-10 17:15:59 +0100

    3995 test

--- GeoImageViewer/src/main/java/org/geoimage/viewer/core/io/WKTString.java
+++ GeoImageViewer/src/main/java/org/geoimage/viewer/core/io/WKTString.java
@@ -3,7 +3,7 @@ package org.geoimage.viewer.core.io;
 
 public class WKTString {
 
-	public static final String WKT3995="PROJCS[\"Stereographic_North_Pole\","
+	public static final String WKT3995_ESRI="PROJCS[\"Stereographic_North_Pole\","
 			+ "    GEOGCS[\"GCS_WGS_1984\","
 			+ "        DATUM[\"WGS_1984\","
 			+ "            SPHEROID[\"WGS_84\",6378137,298.257223563]],"
@@ -16,6 +16,30 @@ public class WKTString {
 			+ "    PARAMETER[\"false_northing\",0],"
 			+ "    UNIT[\"Meter\",1]]"
 			+ ""; 
+	
+	
+	public static final String WKT3995="PROJCS[\"WGS 84 / Arctic Polar Stereographic\","
+			+ "    GEOGCS[\"WGS 84\","
+			+ "    DATUM[\"WGS_1984\","
+			+ "    SPHEROID[\"WGS 84\",6378137,298.257223563,"
+			+ "    AUTHORITY[\"EPSG\",\"7030\"]],"
+			+ "            AUTHORITY[\"EPSG\",\"6326\"]],"
+			+ "        PRIMEM[\"Greenwich\",0,"
+			+ "            AUTHORITY[\"EPSG\",\"8901\"]],"
+			+ "        UNIT[\"degree\",0.0174532925199433,"
+			+ "            AUTHORITY[\"EPSG\",\"9122\"]],"
+			+ "        AUTHORITY[\"EPSG\",\"4326\"]],"
+			+ "    PROJECTION[\"Polar_Stereographic\"],"
+			+ "    PARAMETER[\"latitude_of_origin\",71],"
+			+ "    PARAMETER[\"central_meridian\",0],"
+			+ "    PARAMETER[\"scale_factor\",1],"
+			+ "    PARAMETER[\"false_easting\",0],"
+			+ "    PARAMETER[\"false_northing\",0],"
+			+ "    UNIT[\"metre\",1,"
+			+ "        AUTHORITY[\"EPSG\",\"9001\"]],"
+			+ "    AXIS[\"X\",EAST],    AXIS[\"Y\",NORTH],"
+			+ "    AUTHORITY[\"EPSG\",\"3995\"]]\" ";
+	
 	
 	public static final String WKTWGS84 = ""
 			+ "	GEOGCS[\"WGS 84\","
