66792d577422cf3c41f086d091a022d1539e027f
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2016-03-29 13:40:40 +0200

    start.bat

--- script/start.bat
+++ script/start.bat
@@ -1,4 +1,5 @@
 # SET GDAL_DATA="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal-data"
 # SET GDAL_DRIVER_PATH="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal/plugin"
 # SET LD_LIBRARY_PATH="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal/java"
-java -client -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -XX:NewRatio=2 -Xmx3072m -Djava.library.path="%CD%/lib/gdal_lib_win64/bin/gdal/java;%CD%/lib/gdal_lib_win64/bin/gdal/plugins/" -Dncsa.hdf.hdf5lib.H5.hdf5lib="%CD%/lib/jhdf5dll-2.11.0.dll" -jar sumo.jar
+.\jre\bin\java -Dlog4j.configurationFile=log4j2.xml -client -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -XX:NewRatio=2 -Xmx3072m -Djava.library.path="%CD%/lib/" -Dncsa.hdf.hdf5lib.H5.hdf5lib="%CD%/lib/jhdf5dll-2.11.0.dll" -jar sumo.jar
+#;%CD%/lib/gdal_lib_win64/bin/gdal/plugins/
