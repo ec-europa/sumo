efb2262c379c08c5f6490b63f027ddd55c55e878
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2016-03-31 17:28:46 +0200

    ice null fix

--- script/start_batch.sh
+++ script/start_batch.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-java -Dlog4j.configurationFile=./log4j2.xml -Dsun.java2d.noddraw=true -Dncsa.hdf.hdf5lib.H5.hdf5lib=$install_folder"/lib/libjhdf5.so"  -XX:NewRatio=2 -Xmx3072m -cp ./sumo.jar org.geoimage.viewer.core.batch.Sumo $1 $2
+install_folder=$(pwd)
+GDAL_DRIVER_PATH=$install_folder/lib/
+java -Dlog4j.configurationFile=./log4j2.xml -Dsun.java2d.noddraw=true -Djava.library.path=$install_folder"/lib/" -Dncsa.hdf.hdf5lib.H5.hdf5lib=$install_folder"/lib/libjhdf5.so"  -XX:NewRatio=2 -Xmx3072m -cp ./sumo.jar org.geoimage.viewer.core.batch.Sumo $1 $2
