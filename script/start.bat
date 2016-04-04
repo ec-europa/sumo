eea519c4d48197a32fd0c8a4795c34bfe90f563d
Pietro Argentieri <pietro.argentieri@ext.jrc.ec.europa.eu>
2016-04-04 15:02:07 +0200

    script gdal update

--- script/start.bat
+++ script/start.bat
@@ -1,5 +1,4 @@
-# SET GDAL_DATA="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal-data"
-# SET GDAL_DRIVER_PATH="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal/plugin"
-# SET LD_LIBRARY_PATH="C:/Workspaces_MyEclipse Professional 2014/AlosReader/gdalbin/bin/gdal/java"
-.\jre\bin\java -Dlog4j.configurationFile=log4j2.xml -client -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -XX:NewRatio=2 -Xmx3072m -Djava.library.path="%CD%/lib/" -Dncsa.hdf.hdf5lib.H5.hdf5lib="%CD%/lib/jhdf5dll-2.11.0.dll" -jar sumo.jar
-#;%CD%/lib/gdal_lib_win64/bin/gdal/plugins/
+SET PATH=%PATH%;%CD%\sumo_lib\gdal_lib_win64;%CD%\sumo_lib\gdal_lib_win64\gdal\plugins
+SET GDAL_DRIVER_PATH=%CD%\sumo_lib\gdal_lib_win64
+.\jre\bin\java -Dsun.java2d.noddraw=true -XX:NewRatio=2 -Xmx3072m -Djava.library.path="./;%CD%/sumo_lib/;%CD%\sumo_lib\hdf5\jarhdf5-2.10.1.jar;%CD%/sumo_lib/gdal_lib_win64/;%CD%/sumo_lib/gdal_lib_win64/gdal/java/;%CD%/sumo_lib/gdal_lib_win64/gdal/plugins/" -Dncsa.hdf.hdf5lib.H5.hdf5lib="%CD%\sumo_lib\hdf5\jhdf5.dll"  -jar sumo.jar
+
