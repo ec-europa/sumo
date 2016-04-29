e36b108c8184e33920e4c2097c4f0d2a700c2c80
Pietro <pietro.argentieri@gmail.com>
2016-04-29 02:07:21 +0200

    image layer single thread test

--- GeoImageViewer/src/main/java/org/geoimage/viewer/core/layers/image/ImageLayer.java
+++ GeoImageViewer/src/main/java/org/geoimage/viewer/core/layers/image/ImageLayer.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
